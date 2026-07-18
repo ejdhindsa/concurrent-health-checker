54b6a3c6350610ade32d31d706df0e0bca1c4558
ejdhindsa <ejdhindsa@Gmail.com>
2026-07-18 16:56:17 -0500

    create proper tests

--- main_test.go
+++ main_test.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-// known integration test that can be made better with httptest
-// will be revisited later with httptest import
 func TestGet(t *testing.T) {
-	resp, _, err := Get("https://www.google.com/")
-	if err != nil {
-		t.Fatalf("Expected no errors, go %v", err)
+	tests := []struct {
+		name           string
+		testURL        string
+		mockStatusCode int
+		expectError    bool
+	}{
+		{"Valid 200 OK", "", http.StatusOK, false},
+		{"Not Found 404", "", http.StatusNotFound, false},
+		{"Server Error 500", "", http.StatusInternalServerError, false},
+		{"Network Failure", "http://this-is-not-a-real-website-ever.com", 0, true},
 	}
 
-	if resp.StatusCode != 200 {
-		t.Errorf("Expected status code 200, recieved %d", resp.StatusCode)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tc.mockStatusCode)
+			}))
+			defer server.Close()
+
+			urlToTest := server.URL
+			if tc.testURL != "" {
+				urlToTest = tc.testURL
+			}
+
+			resp, _, err := Get(urlToTest)
+
+			if tc.expectError && err == nil {
+				t.Errorf("Expected an error, but got none")
+			}
+
+			if !tc.expectError && err != nil {
+				t.Errorf("Did not expect an error, but got: %v", err)
+			}
+
+			if resp != nil && tc.mockStatusCode != 0 && resp.StatusCode != tc.mockStatusCode {
+				t.Errorf("Expected status %d, got %d", tc.mockStatusCode, resp.StatusCode)
+			}
+		})
 	}
 }
